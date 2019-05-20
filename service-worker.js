aa14681dc53cb21e8bead9b07384ced8c5b3298d
BLTDESIGN <beltramo.dan@gmail.com>
2019-05-20 11:43:11 -0700

    Updated Finplan Mockups

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3c7d9535122442c44106ee9a42299e00.js"
+  "/precache-manifest.823396c05b7addd3c2f8a43b64bc6150.js"
 );
 
 workbox.clientsClaim();
