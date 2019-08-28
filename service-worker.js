5843c97c6830375b64b21705c93250a3edfa3317
BLTDESIGN <beltramo.dan@gmail.com>
2019-08-27 22:46:07 -0700

    Updated Mockups

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.52d61bf5b89051858426789adb6133f5.js"
+  "/precache-manifest.737815c7aa28c719949e73c864b62dbf.js"
 );
 
 workbox.clientsClaim();
