2eb1b3e3ae67d63f82aedf75e736b879a271584d
BLTDESIGN <beltramo.dan@gmail.com>
2019-12-16 15:24:00 -0800

    Updated Dropdown Styling

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e94bce17c3aaca5477c5636fd9fd509e.js"
+  "/precache-manifest.b0d195721175d6e2239eba06af09eb86.js"
 );
 
 workbox.clientsClaim();
