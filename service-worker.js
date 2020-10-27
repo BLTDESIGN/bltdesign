0224a6eaddaf74ce82c8959ae99a03a505d6badb
BLTDESIGN <beltramo.dan@gmail.com>
2020-10-26 18:29:47 -0700

    Updated project order

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b193ade8a1a25a5191ad8bd6deb791de.js"
+  "/precache-manifest.e3af80df5f7781efc48779c07db62f5e.js"
 );
 
 workbox.clientsClaim();
