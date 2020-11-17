787c9264e72f084e65aea0a615413a3d43165cfb
BLTDESIGN <beltramo.dan@gmail.com>
2020-11-17 15:00:33 -0800

    Updated mobile text size

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1959348b3a9c16610d6bd4c4c2e25fa7.js"
+  "/precache-manifest.7f426c45fa05a7d9f3145580c40ff2f3.js"
 );
 
 workbox.clientsClaim();
