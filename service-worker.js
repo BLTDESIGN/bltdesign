8936241af09e06d580ce1c9d78d6a640e308a0c3
BLTDESIGN <BELTRAMO.DAN@GMAIL.COM>
2021-07-24 16:27:51 -0700

    Updated

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fbab6ee2b1c1d8493990fbfb221f74a2.js"
+  "/precache-manifest.a3346a4eb2c34f068c677ede1c351adf.js"
 );
 
 workbox.clientsClaim();
